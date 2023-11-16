d75e7d8ae3dda7af2899c49fd2bf5fecde6f968f
Dustin Selman <dselman@gmail.com>
2023-11-16 03:30:48 -0800

    Update klor.h

--- miryoku/mapping/44/klor.h
+++ miryoku/mapping/44/klor.h
@@ -4,27 +4,32 @@
 #if !defined (MIRYOKU_LAYOUTMAPPING_KLOR)
 
 
-ZMK_MACRO(NAV_LEFT,
-   wait-ms = <30>;
-   tap-ms = <40>;
-   bindings 
-     = <&macro_press   &kp LGUI &kp LCTRL >
-     , <&macro_tap     &kp LEFT>
-     , <&macro_release &kp LGUI &kp LCTRL>
-     ;
-)
+/ {
+    macros {
+        // 0 params macro
+        nav_left: nav_left {
+            // ...
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>; // Must be 0
+            bindings 
+              = <&macro_press   &kp LGUI &kp LCTRL >
+              , <&macro_tap     &kp LEFT>
+              , <&macro_release &kp LGUI &kp LCTRL>
+              ;
+        };
 
-
-ZMK_MACRO(NAV_RIGHT,
-   wait-ms = <30>;
-   tap-ms = <40>;
-      bindings 
-     = <&macro_press   &kp LGUI &kp LCTRL >
-     , <&macro_tap     &kp RIGHT>
-     , <&macro_release &kp LGUI &kp LCTRL>
-     ;
-)
-
+       nav_right: nav_right {
+            // ...
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>; // Must be 0
+            bindings 
+              = <&macro_press   &kp LGUI &kp LCTRL >
+              , <&macro_tap     &kp RIGHT>
+              , <&macro_release &kp LGUI &kp LCTRL>
+              ;
+        };
+    };
+};
 
 #define XXX &none
 
@@ -37,7 +42,7 @@ ZMK_MACRO(NAV_RIGHT,
      K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09 \
 XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34  &macro NAV_LEFT       &macro NAV_RIGHT  K35  K36  K37
+               K32  K33  K34  &nav_left       &nav_right  K35  K36  K37
 
 #endif
 
