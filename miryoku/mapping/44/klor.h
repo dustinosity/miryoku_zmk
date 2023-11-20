eaff2081f40ffa92ae6d2b8ce749ac9cd83ea4b4
Dustin Selman <dselman@gmail.com>
2023-11-19 20:46:53 -0800

    Update klor.h

--- miryoku/mapping/44/klor.h
+++ miryoku/mapping/44/klor.h
@@ -14,7 +14,7 @@
 ) \
      K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09 \
 &kp C_VOLUME_UP  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  &kp LG(L) \
-&kp C_VOLUME_DOWN  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  &kp C_PLAY_PAUSE \
+&kp C_VOLUME_DOWN  K20  K21  K22  K23  K24  &td0       XXX  K25  K26  K27  K28  K29  &kp C_PLAY_PAUSE \
                K32  K33  K34  &mt LC(LS(LALT)) LG(LC(LEFT))      &mt LC(LS(LALT)) LG(LC(RIGHT))  K35  K36  K37
 
 #endif
