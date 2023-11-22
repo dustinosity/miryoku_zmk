95f61de9bba90eb62a828c2b493a81d82da1f3fb
Dustin Selman <dselman@gmail.com>
2023-11-22 11:28:21 -0800

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALTERNATIVES_MOUSE \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          LC(U_BTN2),        LC(U_BTN3),        LC(U_BTN1),        U_NA,              U_NU,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            \
+U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_NU,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
