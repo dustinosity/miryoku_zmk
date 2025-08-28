227891353f29e039bdbb01195735c26bcba4fa84
Dustin Selman <dselman@gmail.com>
2025-08-28 15:34:46 -0700

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,16 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+&led_strip {
+    chain-length = <21>;
+};
+
+&sensors {
+    left_encoder {
+        triggers-per-rotation = <15>;
+    };
+
+    right_encoder {
+        triggers-per-rotation = <15>;
+    };
+};
